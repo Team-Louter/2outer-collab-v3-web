517fc04981cdcb07f629a18571de550487d94b01
전수안 <dgsw.ahn@dgsw.hs.kr>
2025-09-22 23:49:55 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import ProjectMain from './pages/projectMain'
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path='/:teamname' element={<ProjectMain />} />
+      </Routes>
     </>
   )
 }
