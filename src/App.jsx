caa1dee4adc964ac6f81926aa9b69ad2768e0294
hyxx-su <legendary9899@dgsw.hs.kr>
2025-09-24 01:16:04 +0900

    fix :: svg to img change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
-import Header from './components/header'
 
 function App() {
 
   return (
     <>
-      <Header/>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
