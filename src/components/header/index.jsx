fa33f6b4053308569d0df089e62b42fa6670e7ef
hyxx-su <legendary9899@dgsw.hs.kr>
2025-09-24 15:19:50 +0900

    fix :: Link, text_setting

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,20 +1,27 @@
 // Module.css import
-import styles from './header.module.css'
+import styles from './header.module.css';
+
+// Link import
+import { Link } from 'react-router-dom';
 
 // Img import
-import logo_img from '../../assets/header/Collab_Logo.svg'
-import search_img from '../../assets/header/search.svg'
-import chatting_img from '../../assets/header/chatting.svg'
-import bell_img from '../../assets/header/bell.svg'
-import profile_img from '../../assets/header/profile.svg'
+import logo_img from '../../assets/header/Collab_Logo.svg';
+import search_img from '../../assets/header/search.svg';
+import chatting_img from '../../assets/header/chatting.svg';
+import bell_img from '../../assets/header/bell.svg';
+import profile_img from '../../assets/header/profile.svg';
 
+// Userid
+const userid = 1;
+
+// Const
 const Header = () => {
     return (
         <>
             <header className={styles.main_header}>
-                <a className={styles.logo_button} href="../main">
+                <Link className={styles.logo_button} to={`/`}>
                     <img className={styles.logo_img} src={logo_img} alt="logo" />
-                </a>
+                </Link>
                 
                 <div className={styles.header_search}>
                     <img className={styles.search_img} src={search_img} alt="search" />
@@ -30,17 +37,24 @@ const Header = () => {
                         <img className={styles.bell_img} src={bell_img} alt="bell" />
                     </li>
 
-                    <li className={styles.signup_button}>회원가입</li>
+                    <li className={styles.signup_button}>
+                        <Link className={styles.text_setting} to={`/signup`}>회원가입</Link>
+                    </li>
 
-                    <li className={styles.login_button}>로그인</li>
+                    <li className={styles.login_button}>
+                        <Link className={styles.text_setting} to={`/login`}>로그인</Link>
+                    </li>
 
                     <li className={styles.profile_button}>
-                        <img className={styles.profile_img} src={profile_img} alt="profile" />
+                        <Link className={styles.profile_img} to={`/profile/${userid}`}>
+                            <img src={profile_img} alt="profile" />
+                        </Link>
                     </li>
                 </ul>
             </header>
         </>
-    )
-}
+    );
+};
 
+// Export
 export default Header;
