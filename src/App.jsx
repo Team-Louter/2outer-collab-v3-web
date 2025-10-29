40869fa3c8bd5692931fc69488f7ae3b405e7911
한승환 <coderrornotfound@gmail.com>
2025-10-29 19:57:07 +0900

    Revert "Feature/notice page"

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import UserProfile from './pages/UserProfile';
-import ProjectSetting from './pages/projectSetting';
-import Schedule from './pages/schedule';
-import ProjectMain from './pages/projectMain';
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path='/:teamname' element={<ProjectMain />} />
-        <Route path='/:teamname/schedule' element={<Schedule />} />
-        <Route path='/profile/:userId' element={<UserProfile />} />
-        <Route path='/:teamname/setting' element={<ProjectSetting />} />
-      </Routes>
     </>
   );
 };
