62613b04f37833675493c6bf6734064633f4748c
한승환 <coderrornotfound@gmail.com>
2025-11-21 14:13:49 +0900

    fix: 회의록 아이콘 경로 수정

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -13,16 +13,16 @@ axiosInstance.interceptors.response.use(
   (res) => res,
   (err) => {
     // 로그인 요청에서의 401 에러는 인터셉터에서 처리하지 않음
-    const isLoginRequest = err.config?.url?.includes('/auth/login');
-    
+    const isLoginRequest = err.config?.url?.includes("/auth/login");
+
     if (err.response?.status === 401 && !isLoginRequest) {
       console.warn("인증이 만료되었거나 쿠키가 없습니다.");
       // 로그인 상태 제거
-      localStorage.removeItem('isLoggedIn');
+      localStorage.removeItem("isLoggedIn");
       // 세션 만료 플래그 설정
-      localStorage.setItem('sessionExpired', 'true');
+      localStorage.setItem("sessionExpired", "true");
       // 필요시 로그인 페이지로 리다이렉트
-      window.location.href = '/login';
+      window.location.href = "/login";
     }
     return Promise.reject(err);
   }
